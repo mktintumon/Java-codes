9bd652768d5a7060ba8c2394837aa08194d53392
mktintumon <mktintumon@gmail.com>
2023-02-22 18:05:56 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test{
+    static String myName(){
+        String name = "Mohit kumar";
+        return name;
+    }
+
+    public static void main(String[] args) {
+        String name = myName();
+        System.out.println("My name is " + name);   
+    }
+}
